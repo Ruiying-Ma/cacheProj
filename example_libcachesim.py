1581ddaa064b3f533129a9009cd5cca12d5472eb
Ruiying-Ma <ruiying021120@gmail.com>
2025-02-20 14:47:18 +0800

    initial commit

---
+++ example_libcachesim.py
@@ -0,0 +1,23 @@
+from utils import run_libcachesim, tune_libcachesim
+import os
+import logging
+import logging_config
+
+if __name__ == "__main__":
+    mr = run_libcachesim(
+        cache_trace=os.path.join(os.path.dirname(os.path.abspath(__file__)), "cache", "trace", "zipf", "alpha1_m100_n1000", "0.oracleGeneral.bin"),
+        cache_alg="tinyLFU",
+        cache_cap=9,
+        params="main-cache=clock,window-size=0.1" # if you want to use default parameters, simply set params as "" (an emtpy string)
+    )
+    logging.info(f"Simulate: mr = {mr}")
+
+    mr_info = tune_libcachesim(
+        trace=os.path.join(os.path.dirname(os.path.abspath(__file__)), "cache", "trace", "zipf", "alpha1_m100_n1000", "0.oracleGeneral.bin"),
+        alg="tinyLFU",
+        cache_cap=9
+    )
+    if mr_info != None:
+        logging.info(f"Tune: default_mr = {mr_info[0]}, tuned_mr = {mr_info[1]}, default_params = {mr_info[2]}, tuned_params = {mr_info[3]}")
+    else:
+        logging.info(f"Tune: mr = None")
